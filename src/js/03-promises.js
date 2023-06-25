e3fc771dbcd0668554662b9b17cc0571764107ea
heilya <123752915+heilya@users.noreply.github.com>
2023-06-25 22:40:32 +0200

    task 3 - done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay)
+  });
+};
+promisesForm = document.querySelector('.form');
+
+
+promisesForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+    const {
+    elements: { delay, step, amount }
+    } = event.currentTarget;
+  const quantityPromises = amount.value;
+  let delayValue = Number(delay.value);
+  for (let i = 1; i <= quantityPromises; i++) {
+        createPromise(i, delayValue).then(value => Notiflix.Notify.success(value)).catch(err => Notiflix.Notify.failure(err));
+    delayValue += Number(step.value);
   }
+  promisesForm.reset()
+
 }
+
+
+
